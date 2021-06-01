9be2586af0cd954f6766dab88e2943693aa82371
fardin2689 <fakurian.fardin@gmail.com>
2021-06-01 17:35:41 +0430

    add typeorm, config, product  item

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeormConfig), ProductModule],
 })
 export class AppModule {}
